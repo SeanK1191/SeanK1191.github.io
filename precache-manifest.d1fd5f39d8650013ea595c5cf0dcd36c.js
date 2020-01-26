aa130adeb48e8c44391073da2fda1af45e1aec85
Sean Kennedy <sean-kennedy1191@hotmail.co.uk>
2020-01-26 16:41:13 +0000

    Updates

---
+++ precache-manifest.d1fd5f39d8650013ea595c5cf0dcd36c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e0b774ab8ddad7601721b9bab0015f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68a87aa497f36409f536",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6c4e9ab65bd7c42d52a2",
+    "url": "/static/js/2.4ddbc91a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4ddbc91a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68a87aa497f36409f536",
+    "url": "/static/js/main.3b0b8653.chunk.js"
+  },
+  {
+    "revision": "5db6dfe7a8e2b4135dd2",
+    "url": "/static/js/runtime-main.aa739829.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
