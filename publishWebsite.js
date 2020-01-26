6bb6d7f4da55e3da755a418bcce5a147b7b69cef
Sean Kennedy <sean-kennedy1191@hotmail.co.uk>
2020-01-26 16:58:47 +0000

    Made change

--- publishWebsite.js
+++ publishWebsite.js
@@ -1,5 +1,8 @@
 var ghpages = require('gh-pages');
+const simpleGit = require('simple-git');
  
 ghpages.publish('build', {
-    branch: 'master'
-}, function(err) {});
+    branch: 'master',
+}, function(err) {
+    console.log('Something went wrong', err);
+});
